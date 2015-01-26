feead6e2f57aaabfe15900736d6a8c7649ee2c0f
jeremycrussell <jeremycrussell@gmail.com>
2015-01-26 07:48:05 -0600

    Update apache_install.pp

--- manifests/apache_install.pp
+++ manifests/apache_install.pp
@@ -13,7 +13,7 @@ class phabricator::apache_install ($phabdir = $phabricator::params::phabdir,) {
   class { 'apache::mod::ssl':
   }
 
-  apache::vhost { $fqdn:
+  apache::vhost { $phabricator::params::hostname:
     port            => '80',
     docroot         => "${$phabdir}/webroot",
     custom_fragment => template('phabricator/apache-vhost-default.conf.erb'),
